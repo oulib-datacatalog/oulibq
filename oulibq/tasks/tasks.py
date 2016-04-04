cab21039628f7bc1707c5c21bf35f1aa895785f9
Mark Stacy <mbstacy@gmail.com>
2016-04-04 12:35:57 -0500

    removing not wanted docker library

--- oulibq/tasks/tasks.py
+++ oulibq/tasks/tasks.py
@@ -1,7 +1,7 @@
 from celery.task import task
-from dockertask import docker_task
+#from dockertask import docker_task
 from subprocess import call,STDOUT
-import requests
+#import requests
 
 #Default base directory 
 #basedir="/data/static/"
