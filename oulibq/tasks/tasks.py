d5693fb7cfe4dffc2c866f97ba0286c373fa865a
Mark Stacy <mbstacy@gmail.com>
2017-02-28 16:49:50 -0600

    json library added

--- oulibq/tasks/tasks.py
+++ oulibq/tasks/tasks.py
@@ -7,7 +7,7 @@ from subprocess import call,STDOUT
 #import requests
 import os, hashlib, bagit
 #from pymongo import MongoClient
-import boto3,shutil,requests
+import boto3,shutil,requests,json
 from pandas import read_csv
 #Default base directory 
 #basedir="/data/static/"
