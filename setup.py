282b0f7bf67137149e04a50d438c746ce2839979
Mark Stacy <mbstacy@gmail.com>
2017-05-08 12:10:25 -0500

    update requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,15 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='oulibq',
-      version='0.0',
+      version='0.1.1.1',
       packages= find_packages(),
+      install_requires=[
+          'celery==3.1.22',
+          'pymongo==3.2.1',
+          'requests==2.9.1',
+          'boto3==1.4.0',
+          'pandas==0.18.0',
+          'bagit==1.5.4',
+          'awscli==1.11.0'
+      ],
 )
