f6c96cc15eaccef44e8fc9f796af52e9ff836367
Mark Stacy <mbstacy@gmail.com>
2017-06-05 13:11:26 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='oulibq',
-      version='0.1.6.8',
+      version='0.1.6.9',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
