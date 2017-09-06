fa700c731d4ed3c9d601a969c8f6a50583ef784a
Mark Stacy <mbstacy@gmail.com>
2017-09-05 22:25:17 -0500

    refactored subtasks with queue set

--- oulibq/tasks/replicate_bag.py
+++ oulibq/tasks/replicate_bag.py
@@ -78,21 +78,21 @@ def replicate_bag(bag, project=None, department=None, force=None, celery_queue="
     
     # norfile validation
     if not inventory_metadata['locations']['norfile']['valid'] or force:
-        bag_chain.append(copy_bag.si(bag,nas_bagit,norfile_bagit)) 
-        subtasks.append(validate_norfile_bag.si(bag,norfile_bagit))
+        bag_chain.append(copy_bag.si(bag,nas_bagit,norfile_bagit).set(queue=celery_queue)) 
+        subtasks.append(validate_norfile_bag.si(bag,norfile_bagit).set(queue=celery_queue))
     #  s3 validataion
     if not inventory_metadata['locations']['s3']['valid'] or force:
-        bag_chain.append(upload_bag_s3.si(bag,nas_bagit,s3_bucket,s3_key))
-        subtasks.append(validate_s3_files.si(bag,norfile_bagit,s3_bucket))
+        bag_chain.append(upload_bag_s3.si(bag,nas_bagit,s3_bucket,s3_key).set(queue=celery_queue))
+        subtasks.append(validate_s3_files.si(bag,norfile_bagit,s3_bucket).set(queue=celery_queue))
     # nas validation
-    subtasks.append(validate_nas_files.si(bag,nas_bagit))
+    subtasks.append(validate_nas_files.si(bag,nas_bagit).set(queue=celery_queue))
     
     if len(bag_chain)==2:        
-        cp_val_chain = (bag_chain[0]|bag_chain[1]|subtasks[0]|subtasks[1]|subtasks[2]|clean_nas_files.si())(queue=celery_queue)
+        cp_val_chain = (bag_chain[0]|bag_chain[1]|subtasks[0]|subtasks[1]|subtasks[2]|clean_nas_files.si().set(queue=celery_queue))()
     elif len(bag_chain)==1:
-        cp_val_chain = (bag_chain[0]|subtasks[0]|subtasks[1]|clean_nas_files.si())(queue=celery_queue)
+        cp_val_chain = (bag_chain[0]|subtasks[0]|subtasks[1]|clean_nas_files.si().set(queue=celery_queue))()
     else:
-        cp_val_chain = (subtasks[0] | clean_nas_files.si())(queue=celery_queue)
+        cp_val_chain = (subtasks[0] | clean_nas_files.si().set(queue=celery_queue))()
 
     return "Replication workflow started for bag {0}. Please see child subtasks for workflow result.".format(bag)
 
