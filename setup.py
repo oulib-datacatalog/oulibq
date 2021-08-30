f1f06d1200f1791b8487067a51948edd84963b0e
Tyler Pearson <tdpearson@ou.edu>
2021-08-30 16:22:43 -0500

    update deps

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='oulibq',
-      version='0.1.15',
+      version='0.1.16',
       packages= find_packages(),
       install_requires=[
-          'celery==3.1.22',
-          'pymongo==3.2.1',
-          'requests==2.24.0',
-          'boto3==1.4.0',
+          'celery',
+          'pymongo',
+          'requests==2.26.0',
+          'boto3==1.17.112',
           'pandas',
-          'bagit==1.5.4',
-          'awscli==1.11.0'
+          'bagit==1.8.1',
+          'awscli==1.19.112'
       ],
 )
