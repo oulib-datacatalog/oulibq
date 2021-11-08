a6a2ff29ef5356a8704e174be5a675026f767835
Tyler Pearson <tdpearson@users.noreply.github.com>
2021-11-08 17:48:13 -0600

    update boto3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 #ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(name='oulibq',
-      version='0.1.16',
+      version='0.1.17',
       packages= find_packages(),
       install_requires=[
           'celery==3.1.22',
           'pymongo==3.2.1',
           'requests==2.24.0',
-          'boto3==1.4.0',
+          'boto3==1.17.112',
           'pandas',
           'bagit==1.5.4',
           'awscli==1.11.0'
