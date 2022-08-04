beb1f79cb278618a838a1d0239a229cb362a43df
Rithsek99 <rithsek99@gmail.com>
2022-08-04 17:04:59 -0500

    update test_workflow

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -1,2 +1,21 @@
+from copy import deepcopy
+from requests import patch
 from oulibq.tasks.workflows import replicate, managed_replication
+from oulibq.tasks.config import BAG_LOCATIONS
+import pytest
+
+import bagit
+from six import PY2
+
+if PY2:
+    from mock import MagicMock, Mock, patch
+else:
+    from unittest.mock import MagicMock, Mock, patch
+
+@patch('oulibq.tasks.workflows.find_bag')
+@patch('')
+def test_replicate(mock_find_bag):
+    mock_find_bag.return_value = deepcopy(BAG_LOCATIONS)
+
+    
 
