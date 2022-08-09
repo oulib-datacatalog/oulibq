a7588d3c1246de6446cde3ea3f0f057273b2fc62
Rithsek99 <rithsek99@gmail.com>
2022-08-09 14:01:02 -0500

    update test workflow

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -11,11 +11,3 @@ if PY2:
     from mock import MagicMock, Mock, patch
 else:
     from unittest.mock import MagicMock, Mock, patch
-
-@patch('oulibq.tasks.workflows.find_bag')
-@patch('')
-def test_replicate(mock_find_bag):
-    mock_find_bag.return_value = deepcopy(BAG_LOCATIONS)
-
-    
-
