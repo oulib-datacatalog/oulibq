51f0115210d872adb2206ba80b5b7b8534a57ac0
Tyler Pearson <tdpearson@ou.edu>
2022-08-09 16:37:35 -0500

    fixed delivery_info mock and added first workflow test

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -12,15 +12,14 @@ if PY2:
 else:
     from unittest.mock import MagicMock, Mock, patch
 
-@pytest.mark.skip(reason="failing to mock self.request.delivery_info")
+
 @patch('oulibq.tasks.workflows.get_metadata')
-@patch('oulibq.tasks.workflows.task')
 @patch('oulibq.tasks.workflows.chain')
 @patch('oulibq.tasks.workflows.group')
 @patch('oulibq.tasks.workflows.find_bag')
-def test_replicate(mock_find_bag, mock_celery_group, mock_celery_chain, mock_task, mock_get_metadata):
+@patch('oulibq.tasks.workflows.app.Task.request')
+def test_replicate(mock_Task_request, mock_find_bag, mock_celery_group, mock_celery_chain, mock_get_metadata):
 
-    
     copy_metadata = deepcopy(INVENTORY_METADATA)
 
     bag = "test_bag"
@@ -34,12 +33,12 @@ def test_replicate(mock_find_bag, mock_celery_group, mock_celery_chain, mock_tas
     queue_name = "test_queue_name"
 
     # Mock out the celery tasks
-    validate_nas_files = MagicMock()
-    copy_bag = MagicMock()
-    validate_norfile_bag = MagicMock()
-    upload_bag_s3 = MagicMock()
-    validate_s3_files = MagicMock()
-    remove_nas_files = MagicMock()
+    validate_nas_files = patch('oulibq.tasks.workflows.validate_nas_files').start()
+    copy_bag = patch('oulibq.tasks.workflows.copy_bag').start()
+    validate_norfile_bag = patch('oulibq.tasks.workflows.validate_norfile_bag').start()
+    upload_bag_s3 = patch('oulibq.tasks.workflows.upload_bag_s3').start()
+    validate_s3_files = patch('oulibq.tasks.workflows.validate_s3_files').start()
+    remove_nas_files = patch('oulibq.tasks.workflows.remove_nas_files').start()
 
 
     # Mock out the celery signatures of the tasks
@@ -51,12 +50,14 @@ def test_replicate(mock_find_bag, mock_celery_group, mock_celery_chain, mock_tas
     remove_from_nas = remove_nas_files.si(bag).set(queue=queue_name)
 
     mock_find_bag.return_value = (nas_path, norfile_path, s3_bucket, s3_key, s3_folder)
-    
-    #FIXME: This does not work.
-    mock_task.request.delivery_info.return_value = {'routing_key': queue_name}
+    mock_Task_request.delivery_info.return_value = {'routing_key': queue_name}
 
-    copy_metadata["locations"]["norfile"]["exists"] = False
-    copy_metadata["locations"]["s3"]["exists"] = False
+    copy_metadata["locations"]["norfile"]["exists"] = True
+    copy_metadata["locations"]["norfile"]["valid"] = False
     mock_get_metadata.return_value = copy_metadata
     replicate(bag, project, department)
-    upload_to_s3.assert_called()
+    mock_celery_chain.assert_called_with(
+        validate_nas,
+        validate_norfile,
+        remove_from_nas
+    )
